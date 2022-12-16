cf57b7f182e16c93b0b37c4379e572499e618502
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 14:43:16 +0900

    포 와일 문

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,15 @@
-
+import java.util.Scanner; 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HiWorld");
-		System.out.println("hhhHH");
+
+		int i=1;
+		while(i<10) {
+			i = i + 1;
+		}
+		for(int j=1;j<10;j++){
+			System.out.println(7*j);
+		}
+		
 	}
 }
