11f7d9438ea83eeae7d1c4aa4327b9d7543670cf
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 14:57:46 +0900

    if else안에 for

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,8 +8,16 @@ public class HelloWorld {
 		int number = scanner.nextInt();
 		System.out.println("number : " + number);
 		
+		if(number<2) {
+			System.out.println("2보다 낮음");
+		}
+		else if(number>9) {
+			System.out.println("9보다 크다");
+		}
+		else {
 		for(int i=1;i<10;i++) {
 			System.out.println("값은" + i * number);
-			}			
+			}		
+		}
 	}			
 }
