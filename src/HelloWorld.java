d643212c2a50538bf0416e6ca43e12045d1e8a42
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 15:27:41 +0900

    메소드

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,21 +1,23 @@
 import java.util.Scanner; 
 public class HelloWorld {
+	public static int[] caculate(int times) {
+		int[] result = new int[9];
+		for(int i = 0; i<result.length;i++) {
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+	public static void print(int[] result) {
+		for(int i = 0; i<result.length;i++) {
+			System.out.println(result[i]);
+		}
+	}
 
 	public static void main(String[] args) {
-
-		int[] result = new int[9];
-		for(int i= 0; i < result.length; i++) {
-			result[i] = 2 * (i + 1);
+		for(int i = 2; i<10;i++) {
+		int[] result = caculate(i);
+		print(result);
 		}
-		for(int i = 0; i< result.length; i++) {
-			System.out.println(result[i]);
-		}
-		result = new int[9];
-		for(int i= 0; i < result.length; i++) {
-			result[i] = 3 * (i + 1);
-		}
-		for(int i = 0; i< result.length; i++) {
-			System.out.println(result[i]);
-		}
+		
 	}			
 }
