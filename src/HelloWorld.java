fb9f76ebc0595e3c7e37ce749f86c722ff16494f
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-16 15:12:56 +0900

    배열

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,21 +3,19 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("number : " + number);
-		
-		if(number<2) {
-			System.out.println("2보다 낮음");
+		int[] result = new int[9];
+		for(int i= 0; i < result.length; i++) {
+			result[i] = 2 * (i + 1);
 		}
-		else if(number>9) {
-			System.out.println("9보다 크다");
+		for(int i = 0; i< result.length; i++) {
+			System.out.println(result[i]);
 		}
-		else {
-		for(int i=1;i<10;i++) {
-			System.out.println("값은" + i * number);
-			}		
+		result = new int[9];
+		for(int i= 0; i < result.length; i++) {
+			result[i] = 3 * (i + 1);
+		}
+		for(int i = 0; i< result.length; i++) {
+			System.out.println(result[i]);
 		}
 	}			
 }
