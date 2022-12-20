2cfc854960787f5aafc5d3ddd4bc5ca6b1a0173a
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-20 14:02:21 +0900

    스위치 문

--- src/gugudanMain.java
+++ src/gugudanMain.java
@@ -1,9 +1,36 @@
-
+import java.util.*;
 public class gugudanMain {
 	public static void main(String[] args) {
-		for(int i = 2; i<10;i++) {
-		int[] result = HelloWorld.caculate(i);
-		HelloWorld.print(result);
+		
+		int score = 0;
+		char grade = ' ';
+		
+		System.out.println("당신의 점수를 입력하세요.");
+		
+		Scanner scanner = new Scanner(System.in);
+		String tmp = scanner.nextLine();    //화면을 통해 입력받은 내용을 tmp에 저장
+		score = Integer.parseInt(tmp);      //입력받은 문자열(tmp)를 숫자로 변환
+		
+		switch(score/10) {
+		case 10:
+		case 9:
+			grade = 'A';
+			break;
+		case 8:
+			grade = 'B';
+		break;
+		case 7:
+			grade = 'C';
+			break;
+		case 6:
+			grade = 'd';
+			break;
+			default:
+				grade = 'F';
+				
 		}
-	}	
+		System.out.println("당신의 학점은"+grade+"입니다.");
+	}
+	
+		
 }
