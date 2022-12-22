630ee500bd4e5c3998e74020d75000f4948bae18
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-22 17:42:05 +0900

    참조형 변수

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,15 +7,16 @@ public class HelloWorld {
 	
 		Data d = new Data();
 		d.x = 10;
-		System.out.println(d.x);
 		
-		change(d.x);
-		System.out.println("after change d.x");
-		System.out.println("main :"+ d.x);
+		Data d2 = copy(d);  //같은 클래스 안에 있어서 참조변수 new생략
+		System.out.println("d.x : "+ d.x);
+		System.out.println("d2.x : "+ d2.x);
 		
 		}
-		static void change(int x) {
-			x=1000;
-			System.out.println("change"+ x);
+		static Data copy(Data d) {
+			Data tmp = new Data(); //새로운 객체 tmp생성
+			tmp.x = d.x;           //d.x의 값 tmp에 복사  
+			
+			return tmp;            //복사한 객체의 주소를 반환 
 	}
 }
