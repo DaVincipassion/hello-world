93d92063ccade5df70032b6a5327f1a507b985fb
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 06:34:53 +0900

    6-8

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,15 +1,20 @@
 
 class HelloWorld{
 	public static void main(String args[]) {
-
+        System.out.println("main시작");
 		firstMethod();
+		System.out.println("main끝남");
 		
 }
 	static void firstMethod(){
+		System.out.println("first시작");
 		secondMethod();
+		System.out.println("first끝남");
 	}
 	static void secondMethod(){
-		System.out.println("secondMethod()");
+		
+		System.out.println("secondMethod()시작");
+		System.out.println("secondMethod()끝남");
 		
 	}
 }
