9377acc4c84806ced382a968201be2cacb23df98
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 06:06:12 +0900

    6-4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,15 +10,15 @@ class Tv{
 
 class HelloWorld{
 	public static void main(String args[]) {
-		Tv t1 = new Tv();
-		Tv t2 = new Tv();
-		System.out.println("t1의 채널값은 "+t1.channel+"입니다.");
-		System.out.println("t2의 채널값은 "+t2.channel+"입니다.");
-		t2 = t1;         //t2의 주소를 t1에 연결
-		t1.channel = 7;
-		System.out.println("t1의 채널값은"+t1.channel+"으로 변경하였습니다.");
+		Tv tvArr[] = new Tv[3];
 		
-		System.out.println("t1의 채널값은 "+t1.channel+"입니다.");
-		System.out.println("t2의 채널은 "+t2.channel+"입니다.");
+		for(int i= 0; i<tvArr.length;i++) {
+			tvArr[i]= new Tv();
+			tvArr[i].channel = i + 10;
+		}
+		for(int i = 0; i<tvArr.length;i++) {
+			tvArr[i].channelUp();
+			System.out.printf("tvArr[%d].channe=%d%n",i,tvArr[i].channel);
+		}
 	}
 }
