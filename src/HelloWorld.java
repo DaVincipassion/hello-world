4b863d57a1b7db6f62891c4cd9beffa417b41a2b
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 06:43:44 +0900

    6-11

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,17 @@
 class Data{int x;}
 class HelloWorld{
 	public static void main(String args[]) {
-        Data d = new Data();
-        d.x = 10;
-        System.out.println(d.x);
+        int x[]= {10};
         
-        change(d);
+        System.out.println(x[0]);
+        
+        change(x);
         System.out.println("after change");
-        System.out.println("main x"+d.x);
+        System.out.println("main x"+x[0]);
         
 	}
-	static void change(Data d) {
-		d.x = 1000;
-		System.out.println("change:x"+d.x);
+	static void change(int x[]) {
+		x[0] = 1000;
+		System.out.println("change:x"+x[0]);
 	}
 }
