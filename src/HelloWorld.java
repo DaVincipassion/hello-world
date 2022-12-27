140353a4f841843dfdc0503851dcbc9caefb733a
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 05:58:19 +0900

    6-3

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -14,9 +14,11 @@ class HelloWorld{
 		Tv t2 = new Tv();
 		System.out.println("t1의 채널값은 "+t1.channel+"입니다.");
 		System.out.println("t2의 채널값은 "+t2.channel+"입니다.");
+		t2 = t1;         //t2의 주소를 t1에 연결
 		t1.channel = 7;
 		System.out.println("t1의 채널값은"+t1.channel+"으로 변경하였습니다.");
 		
+		System.out.println("t1의 채널값은 "+t1.channel+"입니다.");
 		System.out.println("t2의 채널은 "+t2.channel+"입니다.");
 	}
 }
