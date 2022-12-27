8dea6ee4ba2a91a7ebb1f89743b49e77ed29de66
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 07:23:02 +0900

    6-19

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,21 +2,32 @@
 class HelloWorld{
 	public static void main(String args[]) {
        
-		int x=2;
-		int n=5;
-		long result = 0;
+		System.out.println(MyMath2.add(200L,100L));
+		System.out.println(MyMath2.subtract(200L,100L));
+		System.out.println(MyMath2.multiply(200L,100L));
+		System.out.println(MyMath2.divide(200.0,100.0));
 		
-		for(int i=1;i<=n;i++) {
-			result += power(x,i);
-		}
-	
-		System.out.println(result);
-	}
-     
-
-	static long power(int x,int n) {
-		if(n==1) return x;
-		return x * power(x, n-1);
-	}
+		MyMath2 mm = new MyMath2();
+		mm.a = 200L;
+		mm.b = 100L;
 		
+		System.out.println(mm.add());
+		System.out.println(mm.subtract());
+		System.out.println(mm.multiply());
+		System.out.println(mm.divide());
+	}
 }
+     
+class MyMath2{
+	long a, b;
+	
+	long add() {return a+b;}
+	long subtract() {return a-b;}
+	long multiply() {return a * b;}
+	double divide() {return a/b;}
+	
+	static long add(long a,long b) {return a+b;}
+	static long subtract(long a,long b) {return a-b;}
+	static long multiply(long a,long b) {return a*b;}
+	static double divide(double a,double b) {return a / b;}
+	}
