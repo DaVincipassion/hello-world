0000ad40280c6d26e631412b20dd6099a8df069e
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 07:54:58 +0900

    6-25

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,8 @@ class HelloWorld{
 	public static void main(String args[]) {
       
 		Car c1 = new Car();
-		c1.color = "white";
-		c1.gearType = "auto";
-		c1.door = 4;
 		
-		Car c2 = new Car("White", "auto", 4);
+		Car c2 = new Car("blue");
 		
 		System.out.println(c1.color + c1.gearType + c1.door);
 		System.out.println(c2.color + c2.gearType + c2.door);
@@ -19,11 +16,16 @@ class Car{
 	String gearType;
 	int door;
 	
-	Car(){}
-	Car(String c, String g,int d){
-		color = c;
-		gearType = g;
-		door = d;
+	Car(){
+		this("white","auto",4);
+	}
+	Car(String color){
+		this(color,"auto",4);
+	}
+	Car(String color, String gearType,int door){
+		this.color = color;
+		this.gearType = gearType;
+		this.door = door;
 	}
 }
 	
