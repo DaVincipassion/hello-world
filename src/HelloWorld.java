199269de4882b8ffcf8e4c0b29f380d1bb82c000
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 06:17:04 +0900

    6-5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,24 +1,30 @@
-class Tv{
-	String color;
-	boolean power;
-	int channel;
-	
-	void power() { power = !power; }
-	void channelUp() { ++channel; }
-	void channelDown( ){ --channel; } 
+class Card{
+	String kind;
+	int number;
+	static int width = 100;
+	static int height = 250;
 }
 
 class HelloWorld{
 	public static void main(String args[]) {
-		Tv tvArr[] = new Tv[3];
+		System.out.println("Card.width= "+Card.width);
+		System.out.println("Card.height= "+Card.height);
 		
-		for(int i= 0; i<tvArr.length;i++) {
-			tvArr[i]= new Tv();
-			tvArr[i].channel = i + 10;
-		}
-		for(int i = 0; i<tvArr.length;i++) {
-			tvArr[i].channelUp();
-			System.out.printf("tvArr[%d].channe=%d%n",i,tvArr[i].channel);
-		}
+		Card c1 =new Card();
+		c1.kind = "Heart";
+		c1.number = 7;
+		
+		Card c2 = new Card();
+		c2.kind = "Spade";
+		c2.number= 4;
+		
+		System.out.println(c1.kind+","+c1.number +","+ c1.width + c1.height);
+		System.out.println(c2.kind+","+c2.number +","+ c2.width + c2.height);
+		
+		c1.width = 50;
+		c1.height = 80;
+		
+		System.out.println(c1.kind+c1.number + c1.width + c1.height);
+		System.out.println(c2.kind+c2.number + c2.width + c2.height);
 	}
 }
