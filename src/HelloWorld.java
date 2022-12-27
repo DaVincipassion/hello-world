46f451700229ef2e3a1fdef15cebe68b257f9e57
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-28 06:30:37 +0900

    6-7

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,27 +1,15 @@
-class MyMath{
-	long add(long a, long b) {
-
-		long result = a+b;
-		return result;
-		}
-	long subtrack(long a, long b) {return a-b;}
-	long multiply(long a,long b) {return a*b;}
-	double divide(double a, double b) {return a / b;}
-}
 
 class HelloWorld{
 	public static void main(String args[]) {
 
-		MyMath mm = new MyMath();
-		long result1 = mm.add(5L, 3L);
+		firstMethod();
 		
-		long result2 = mm.subtrack(5L, 3L);
-		long result3 = mm.multiply(5L, 3L);
-		double result4 = mm.divide(5L, 3L);
+}
+	static void firstMethod(){
+		secondMethod();
+	}
+	static void secondMethod(){
+		System.out.println("secondMethod()");
 		
-		System.out.println(result1);
-		System.out.println(result2);
-		System.out.println(result3);
-		System.out.println(result4);
 	}
 }
