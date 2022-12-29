8fdc04ce96777a46c33aece3803bb537446ef499
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-29 19:39:00 +0900

    7-1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,31 +1,30 @@
-
-class HelloWorld{
-	public static void main(String args[]) {
-      
-		Car c1 = new Car();
-		
-		Car c2 = new Car("blue");
-		
-		System.out.println(c1.color + c1.gearType + c1.door);
-		System.out.println(c2.color + c2.gearType + c2.door);
+class Tv{
+	boolean power;
+	int channel;
+	
+	void power() { power = !power;}
+	void channelUp() {++channel;}
+	void channelDown() {--channel;}
+	
+}
+class CaptionTv extends Tv{
+	boolean caption; //캡션상태 (on/off)
+	
+	void displayCaption(String text) {
+		if(caption) {
+			System.out.println(text); //캡션 상태가 on(true)일 떄만 text를 보여 준다.
+		}
 	}
 }
-     
-class Car{
-	String color;
-	String gearType;
-	int door;
-	
-	Car(){
-		this("white","auto",4);
-	}
-	Car(String color){
-		this(color,"auto",4);
-	}
-	Car(String color, String gearType,int door){
-		this.color = color;
-		this.gearType = gearType;
-		this.door = door;
+class HelloWorld{
+	public static void main(String args[]) {
+      	CaptionTv ctv = new CaptionTv();
+      	ctv.channel = 10;
+      	ctv.channelUp();
+      	System.out.println(ctv.channel);
+      	ctv.displayCaption("Hello, World");
+      	ctv.caption = true;   //캡션(자막) 기능을 켠다.
+      	ctv.displayCaption("Hello, World");
 	}
 }
 	
