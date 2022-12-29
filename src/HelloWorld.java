bd0b9bba435aeab810c7f461697a065855e8f788
kimuc <kimuc@DESKTOP-LML26VQ>
2022-12-29 19:57:18 +0900

    7-2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,30 +1,68 @@
-class Tv{
-	boolean power;
-	int channel;
-	
-	void power() { power = !power;}
-	void channelUp() {++channel;}
-	void channelDown() {--channel;}
-	
-}
-class CaptionTv extends Tv{
-	boolean caption; //캡션상태 (on/off)
-	
-	void displayCaption(String text) {
-		if(caption) {
-			System.out.println(text); //캡션 상태가 on(true)일 떄만 text를 보여 준다.
-		}
-	}
-}
+
 class HelloWorld{
 	public static void main(String args[]) {
-      	CaptionTv ctv = new CaptionTv();
-      	ctv.channel = 10;
-      	ctv.channelUp();
-      	System.out.println(ctv.channel);
-      	ctv.displayCaption("Hello, World");
-      	ctv.caption = true;   //캡션(자막) 기능을 켠다.
-      	ctv.displayCaption("Hello, World");
+     
+		Point[] p = { new Point(100, 100),
+				new Point(140, 50),
+				new Point(200, 100)};
+		
+		Triangle t = new Triangle(p);
+		Circle c = new Circle(new Point(150, 150),50);
+		
+		t.draw(); //삼각형을 그린다.
+		c.draw(); //원을 그린다.
+		}
+	}
+class Shape{
+	String color = "black";
+	void draw() {
+		System.out.printf("[color=%s]%n", color);
+	}
+}
+class Point{
+	int x;
+	int y;
+	
+	Point(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+	Point(){
+		this(0,0);
+	}
+	
+	String getXY() {
+		return "("+x+","+y+")";  //x와y의 값을 문자열로 반환
+		
+	}
+}
+class Circle extends Shape{
+	Point center; //원의 원점좌표
+	int r;        //반지름
+	
+	Circle(){
+		this(new Point(0, 0), 100); // Circle(Point center, int r)를 호출
+	}
+	Circle(Point center, int r){
+		this.center = center;
+		this.r =r;
+	}
+	
+	void draw() {
+		System.out.printf("[center=(%d, %d), r=%d, color=%s]%n",center.x, center.y, r, color);
+	}
+	
+}
+
+class Triangle extends Shape{
+	Point[] P = new Point[3];
+	
+	Triangle(Point[] P) {
+		this.P = P;
+	}
+	
+	void draw() {
+		System.out.printf("[p1=%s, p2=%s, p3=%s, color=%s]%n",P[0].getXY(), P[1].getXY(), P[2].getXY(), color);
 	}
 }
 	
