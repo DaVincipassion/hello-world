03e0fc80e6cda5f6c042b4c564a2b8741a15b86f
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-09 21:41:29 +0900

    큐 스택

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,25 @@
+import java.util.*;
 
 class HelloWorld{
-	Object iv = new Object() { void method() {}}; //익명 클래스
-	static Object cv = new Object() {void method(){} };
-	
-	void mtMethod() {
-		Object lv = new Object() {void method(){} };
+	public static void main(String[] args) {
+		Stack st = new Stack();
+		Queue q = new LinkedList(); //큐 인터페이스의 구현체인 링크드리스트 사용
 		
+		st.push("0");
+		st.push("1");
+		st.push("2");
+		
+		q.offer("0");
+		q.offer("1");
+		q.offer("2");
+		
+		System.out.println("= Stack =");
+		while(!st.empty()) {
+			System.out.println(st.pop()); //스택에서 요소 하나를 꺼내서 출력
+		}
+		System.out.println("= Queue =");
+		while(!q.isEmpty()) {
+			System.out.println(q.poll());//큐에서 요소 하나를 꺼내서 출력
+		}
 	}
 }
-	
