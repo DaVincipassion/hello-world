ce2b25bc67a5477c0569270245d9f0954d3ac074
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-09 20:14:22 +0900

    Outer.this.value 여러번 보기

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,31 +1,22 @@
-class Outer2{
-	class InstanceInner{
-		int iv = 100;
-	}
-	static class StaticInner{
-		int iv = 200;
-		static int cv = 300;
-	}
+class Outer3{
+	int value = 10;  //Outer.this.value
 	
-	void myMethod() {
-		class LocalInner{
-			int iv = 400;
+	class Inner{
+		int value = 20;  //this.value
+		
+		void method() {
+			int value = 30;
+			System.out.println("value:"+value);
+			System.out.println("this.value:"+this.value);
+			System.out.println("Outer3.this.value:"+Outer3.this.value);
 		}
 	}
 }
 class HelloWorld{
 	public static void main(String[] args) {
-		//인스턴스클래스의 인스턴스를 생성하려면
-		//외부 클래스의 인스턴스를 먼저 생성해야 한다.
-		Outer2 oc = new Outer2();
-		Outer2.InstanceInner ii = oc.new InstanceInner();
-		
-		System.out.println("ii.iv: "+ ii.iv);
-		System.out.println("Outer2.StaticInner.cv : "+Outer2.StaticInner.cv);
-		
-		//스태틱 내부 클래스의 인스턴스는 외부 클래스를 먼저 생성하지 않아도 된다.
-		Outer2.StaticInner si = new Outer2.StaticInner();
-		System.out.println("si.iv"+si.iv);
+		Outer3 outer = new Outer3();
+		Outer3.Inner inner = outer.new Inner();
+		inner.method();
 	}
 }
 	
