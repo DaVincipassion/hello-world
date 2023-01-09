35863f5bad19e055d9af243810453c837ffa6e84
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-09 21:48:30 +0900

    스택

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,24 +2,35 @@ import java.util.*;
 
 class HelloWorld{
 	public static void main(String[] args) {
-		Stack st = new Stack();
-		Queue q = new LinkedList(); //큐 인터페이스의 구현체인 링크드리스트 사용
-		
-		st.push("0");
-		st.push("1");
-		st.push("2");
-		
-		q.offer("0");
-		q.offer("1");
-		q.offer("2");
-		
-		System.out.println("= Stack =");
-		while(!st.empty()) {
-			System.out.println(st.pop()); //스택에서 요소 하나를 꺼내서 출력
+		if(args.length != 1) {
+			System.out.println("EXPRESSION");
+			System.out.println("((2+3)*1)+3");
+			System.exit(0);
 		}
-		System.out.println("= Queue =");
-		while(!q.isEmpty()) {
-			System.out.println(q.poll());//큐에서 요소 하나를 꺼내서 출력
+		
+		Stack st = new Stack();
+		String expression = args[0];
+		
+		System.out.println("expression:"+ expression);
+		
+		try {
+			for(int i = 0; i< expression.length(); i++) {
+				char ch = expression.charAt(i);
+				
+				if(ch == '(') {
+					st.push(ch + "");
+				}else if(ch ==')') {
+					st.pop();
+				}
+			}
+			if(st.isEmpty()) {
+				System.out.println("괄호가 일치합니다.");
+			}else {
+				System.out.println("괄호가 일치하지 않습니다");
+				
+			}
+		}catch(EmptyStackException e) {
+			System.out.println("괄호가 일치하지 않습니다");
 		}
 	}
 }
