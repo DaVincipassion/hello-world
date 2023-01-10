4620bedbbfdef06b479176df335f3f9f47eeec75
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-10 16:39:41 +0900

    hashset 리스트로 바꿔 정렬 (로또번호)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,23 +3,16 @@ import java.util.*;
 class HelloWorld{
 	public static void main(String[] args) {
 
-		Object[] objArr = {"1",new Integer(1),"2","2","3","3","4","4","4"};
 		Set set = new HashSet();
 		
-		for(int i=0; i < objArr.length;i++) {
-			set.add(objArr[i]); //  HashSet에 objArr의 요소들을 저장한다.
-			
-		}
-		//HashSet에 저장된 요소들을 출력한다.
-		System.out.println(set);
-		
-		//HashSet에 저장된 요소들을 출력한다.(Iterator이용)
-		Iterator it = set.iterator();
-		
-		while(it.hasNext()) {
-			System.out.println(it.next());
+		for(int i=0;set.size() < 6 ; i++) {
+			int num = (int)(Math.random()*45)+1;
+			set.add(new Integer(num));
 		}
 		
+		List list = new LinkedList(set);
+		Collections.sort(list);
+		System.out.println(list);
 	}
 	
 }
