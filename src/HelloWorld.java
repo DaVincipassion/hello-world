834902a518e9e0c8f4ecc9c7212a941012d39717
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-11 20:02:33 +0900

    ArrayList1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,13 +3,48 @@ import java.util.*;
 class HelloWorld{
 	public static void main(String[] args) {
 
-		TreeSet set = new TreeSet();
-		int[] score = {80,95,50,35,45,65,10,100};
+	
+		ArrayList list1 = new ArrayList(10);
+		list1.add(new Integer(5));
+		list1.add(new Integer(4));
+		list1.add(new Integer(2));
+		list1.add(new Integer(0));
+		list1.add(new Integer(1));
+		list1.add(new Integer(3));
 		
-		for(int i = 0; i<score.length; i++)
-			set.add(new Integer(score[i]));
+		ArrayList list2 = new ArrayList(list1.subList(1, 4));
+		print(list1, list2);
 		
-		System.out.println(set.headSet(new Integer(50)));
-		System.out.println(set.tailSet(new Integer(50)));
+		Collections.sort(list1);
+		Collections.sort(list2);
+		print(list1, list2);
+		
+		System.out.println("list1.containsAll(list2)"+list1.retainAll(list2));
+		
+		list2.add("B");
+		list2.add("C");
+		list2.add(3,"A");
+		print(list1, list2);
+		
+		list2.set(3,  "AA");
+		print(list1, list2);
+		
+		//list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
+		System.out.println("list1.retain(list2):"+list1.retainAll(list2));
+		
+		print(list1, list2);
+		
+		//list2에서 list1에 포함된 객체들을 삭제한다.
+		for(int i= list2.size()-1; i>=0;i--) {
+			if(list1.contains(list2.get(i)))
+				list2.remove(i);
+		}
+		print(list1,list2);
+	}
+	static void print(ArrayList list1, ArrayList list2) {
+		System.out.println("list1:"+list1);
+		System.out.println("list2:"+list2);
+		System.out.println();
 	}
 }
+	
