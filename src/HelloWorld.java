10b0cb2e3995f874fc7b72827346f237b40853e1
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-12 18:26:34 +0900

    지내릭

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,50 +1,27 @@
-import java.util.*;
 
+import java.util.*;
+class Product{
+}
+class Tv extends Product{}
+class Audio extends Product{}
 class HelloWorld{
 	public static void main(String[] args) {
 
+		ArrayList<Product> productList = new ArrayList<Product>();
+		ArrayList<Tv> TvList = new ArrayList<Tv>();
 	
-		ArrayList list1 = new ArrayList(10);
-		list1.add(new Integer(5));
-		list1.add(new Integer(4));
-		list1.add(new Integer(2));
-		list1.add(new Integer(0));
-		list1.add(new Integer(1));
-		list1.add(new Integer(3));
+		productList.add(new Tv());
+		productList.add(new Audio());
 		
-		ArrayList list2 = new ArrayList(list1.subList(1, 4));
-		print(list1, list2);
+		TvList.add(new Tv());
+		TvList.add(new Tv());
 		
-		Collections.sort(list1);
-		Collections.sort(list2);
-		print(list1, list2);
+		printAll(productList);
 		
-		System.out.println("list1.containsAll(list2)"+list1.retainAll(list2));
-		
-		list2.add("B");
-		list2.add("C");
-		list2.add(3,"A");
-		print(list1, list2);
-		
-		list2.set(3,  "AA");
-		print(list1, list2);
-		
-		//list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
-		System.out.println("list1.retain(list2):"+list1.retainAll(list2));
-		
-		print(list1, list2);
-		
-		//list2에서 list1에 포함된 객체들을 삭제한다.
-		for(int i= list2.size()-1; i>=0;i--) {
-			if(list1.contains(list2.get(i)))
-				list2.remove(i);
-		}
-		print(list1,list2);
+   }
+	public static void printAll(ArrayList<Product> list) {
+		for(Product p : list)
+			System.out.println(p);
 	}
-	static void print(ArrayList list1, ArrayList list2) {
-		System.out.println("list1:"+list1);
-		System.out.println("list2:"+list2);
-		System.out.println();
-	}
+
 }
-	
