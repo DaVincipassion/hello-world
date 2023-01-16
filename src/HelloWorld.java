3a08dd9e62db9b4159d73be2395ca5134a261e70
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-16 18:22:44 +0900

    예외처리2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,13 +4,15 @@
 	public static void main(String[] args) {
 	
 		System.out.println(1);
+		System.out.println(2);
 		try {
-			System.out.println(0/0);
-			System.out.println(2);
-		}catch(ArithmeticException ae) {
 			System.out.println(3);
+			System.out.println(0/0);
+			System.out.println(4);  //실행되지 않는다.
+		}catch(Exception e) {   
+			System.out.println(5);
 		}
-		System.out.println(4);
+		System.out.println(6);
 	}
 }
 	
