0f0f40da012c2d551ae9b62e59ea718cc53e1284
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-16 18:30:54 +0900

    예외처리 고의로발생throw

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,20 +3,16 @@
   class HelloWorld{
 	public static void main(String[] args) {
 	
-		System.out.println(1);
-		System.out.println(2);
+		
 		try {
-			System.out.println(3);
-			System.out.println(0/0);
-			System.out.println(4);  //실행되지 않는다.
-		}catch(ArithmeticException ae) {
-			if(ae instanceof ArithmeticException) 
-			System.out.println("true");
-			System.out.println("ArithmeticException");
+			Exception e = new Exception("고의로 발생시켰음");
+			throw e;
 		}catch(Exception e) {
-		System.out.println("Exception");
+			e.printStackTrace();
+			System.out.println("예외메시지 : "+e.getMessage());
+			
 	}
-		System.out.println(6);
+		System.out.println("프로그램이 정상 종료되었음");
 	}
 }
 	
