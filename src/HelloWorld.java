f3257c5b53eaf5095392be072099694914d5fd0d
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-16 18:42:38 +0900

    예외처리 파일생성

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,21 @@
+import java.io.File;
 
-
-  class HelloWorld{
+class HelloWorld{
 	public static void main(String[] args) {
 	
-		
-	
-		throw new RuntimeException();
+		try {
+			File f = createFile(args[0]);
+			System.out.println(f.getName()+"파일이 성공적으로 생성되었습니다.");
+		}catch(Exception e) {
+			System.out.println(e.getMessage()+"다시 입력해 주시기 바랍니다.");
+		}
+	}
+	static File createFile(String fileName) throws Exception{
+		if(fileName == null || fileName.equals(""))
+			throw new Exception("파일이름이 유효하지 않습니다.");
+		File f = new File(fileName);
+		f.createNewFile();
+		return f;
 	}
 }
 	
