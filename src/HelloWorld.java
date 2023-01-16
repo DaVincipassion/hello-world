824d3a362dfc7c18ca5bf7846dda5f7db77e645e
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-16 18:32:02 +0900

    예외처리 런타임

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,15 +4,8 @@
 	public static void main(String[] args) {
 	
 		
-		try {
-			Exception e = new Exception("고의로 발생시켰음");
-			throw e;
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("예외메시지 : "+e.getMessage());
-			
-	}
-		System.out.println("프로그램이 정상 종료되었음");
+	
+		throw new RuntimeException();
 	}
 }
 	
