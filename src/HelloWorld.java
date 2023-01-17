ddd154dd7de53b2c4387e88094a1f604186808b6
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-17 18:13:12 +0900

    멀티쓰레드

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,28 +1,25 @@
 
 class HelloWorld{
+	static long startTime = 0;
+
 	public static void main(String[] args) {
 	
-	ThreadEx1 t1 = new ThreadEx1();
-	Runnable r = new ThreadEx2();
-	Thread t2 = new Thread(r);
-	
-	t1.start();
-	t2.start();
-	}
-}
-	
-class ThreadEx1 extends Thread{
-	public void run() {
-		for(int i=0; i<5; i++) {
-			System.out.println(getName());
+		ThreadEx3 th1 = new ThreadEx3();
+		th1.start();
+		startTime = System.currentTimeMillis();
+		
+		for(int i = 0; i<300;i++) 
+			System.out.printf("%s",new String("-"));
 			
+			System.out.print(System.currentTimeMillis()-HelloWorld.startTime);
 		}
 	}
-}
-class ThreadEx2 implements Runnable{
-	public void run() {
-		for(int i = 0;i<5;i++) {
-			System.out.println(Thread.currentThread().getName());
+			class ThreadEx3 extends Thread{
+				public void run() {
+			for(int i=0;i<300;i++) 
+				System.out.printf("%s",new String("|"));
+				
+				System.out.print(System.currentTimeMillis()-HelloWorld.startTime);
+			}
 		}
-	}
-}
+	
