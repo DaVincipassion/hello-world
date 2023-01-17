d25583b36f369ad2a023b3075e00f748bcf94461
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-17 18:20:49 +0900

    멀티쓰레드 입력하면ㅅ ㅓ카운트다운

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,14 @@ import javax.swing.JOptionPane;
 class HelloWorld{
 	public static void main(String[] args)throws Exception {
 	
+		ThreadEx5 th1 = new ThreadEx5();
+		th1.start();
 		String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
 		System.out.println("입력하신 값은 "+ input + "입니다");
-		
+	}
+}
+		class ThreadEx5 extends Thread{
+			public void run() {
 		for(int i=10; i>0;i--) {
 			System.out.println(i);
 			try {
