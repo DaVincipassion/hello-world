fcb584f6eb1959d9982460ec7bc00979cc6c950a
DaVincipassion <git@github.com:DaVincipassion/testscode.git>
2023-01-18 19:49:45 +0900

    sleep쓰레드

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,31 +2,36 @@
 class HelloWorld{
 	public static void main(String[] args)throws Exception {
 	
-		ThreadEx6 th1 = new ThreadEx6();
-		ThreadEx6_2 th2 = new ThreadEx6_2();
-
-		th2.setPriority(7);
-		
-		System.out.println(th1.getPriority());
-		System.out.println(th2.getPriority());
+		RunImplEx10 r = new RunImplEx10();
+		Thread th1 = new Thread(r,"*");
+		Thread th2 = new Thread(r,"**");
+		Thread th3 = new Thread(r,"***");
 		th1.start();
 		th2.start();
+		th3.start();
+		
+		try {
+			Thread.sleep(2000);
+			th1.suspend();
+			Thread.sleep(2000);
+			th2.suspend();
+			Thread.sleep(3000);
+			th1.resume();
+			Thread.sleep(3000);
+			th1.stop();
+			th2.stop();
+			Thread.sleep(2000);
+			th3.stop();
+		}catch(InterruptedException e) {}
 	}
-	
 }
-class ThreadEx6 extends Thread{
+class RunImplEx10 implements Runnable{
 	public void run() {
-		for(int i=0;i<300;i++) {
-			System.out.println("-");
-			for(int x=0;x<10000000;x++);
-		}
-	}
-}
-class ThreadEx6_2 extends Thread{
-	public void run() {
-		for(int i=0;i<300;i++) {
-			System.out.println("|");
-			for(int x=0;x<10000000;x++);
+		while(true) {
+			System.out.println(Thread.currentThread().getName());
+			try {
+				Thread.sleep(1000);
+			}catch(InterruptedException e) {}
 		}
 	}
 }
